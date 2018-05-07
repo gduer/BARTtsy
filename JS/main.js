8d0a5cd5f26dac961a71d3385f51f4679020ecbe
gduer <31167862+gduer@users.noreply.github.com>
2018-05-06 23:03:21 -0400

    idk

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,21 @@
+stations = "https://raw.githubusercontent.com/gduer/BARTtsy/master/bartData.json"
+
+
 var map = L.map('map', {
   center: [37.6918, -122.2001],
   zoom: 10
 });
 
 var Stamen_TonerLite = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  subdomains: 'abcd',
   minZoom: 0,
   maxZoom: 20,
   ext: 'png'
 }).addTo(map);
 
-console.lod(data)
+
+$(document).ready(function() {
+  $.ajax(stations).done(function(data) {
+      var parsedData = JSON.parse(data);
+      _.each(parsedData, function(item){ L.circleMarker([item.geometry.lat,item.geometry.lon]).addTo(map).bindPopup(item.Name);});
+});
+});
